4db5179785a057ee68e75c8ce0b56ca03104edba
oleolerato <olerato.mosikidi@gmail.com>
2022-10-13 17:14:00 +0200

    Get Nodes and declare myLibrary and i variable

---
+++ script.js
@@ -0,0 +1,9 @@
+const popup = document.querySelector('.popup');
+const inputs = document.querySelectorAll('.popup form input');
+const addBookBtn = document.querySelector('.popup form .addBook');
+
+const books = document.querySelector('.books');
+const createEntryBtn = document.querySelector('.books .createEntry')
+
+let myLibrary = [];
+let i = 0;
