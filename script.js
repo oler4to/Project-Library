2cd501c5e762ae675b2ff30a3ef2fe50ec546589
oleolerato <olerato.mosikidi@gmail.com>
2022-10-13 17:26:48 +0200

    Create Book() constructor

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ const books = document.querySelector('.books');
 const createEntryBtn = document.querySelector('.books .createEntry')
 
 let myLibrary = [];
+let storeDetails = {
+  title: "",
+  author: "",
+  pages: 0,
+  read: false,
+}
 let i = 0;
+
+function Book(a){
+  this.title = a.title;
+  this.author = a.author;
+  this.pages = a.pages;
+  this.hasRead = a.read;
+}
+
