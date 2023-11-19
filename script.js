fc7e7fcd6cf461d1ba6dea456c142ab9cb34f985
Olerato. <olerato.mosikidi@gmail.com>
2023-11-19 23:05:53 +0200

    Declare private field in class. Add private field to constructor as IIFE that adds the new instance to Library array as an entry.

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ let Library = [];
 let valueIsMissing = true;
 
 class Book{
- 
+ #addToLibrary
  constructor(entry, title, author, pages, hasRead){
    this.entry = `b${entry}`;
    this.title = title;
    this.author = author;
    this.pages = pages;
    this.hadRead = hasRead;
+   
+     this.#addToLibrary = (() => {
+       Library[this.entry] = this
+      })();
  }
 
 }
